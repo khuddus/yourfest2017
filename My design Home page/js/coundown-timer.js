a472ba9496562f0add6fa17731bdb433fff058b6
DChanakya <dandamudichanakya@gmail.com>
2016-12-26 19:56:40 +0530

    Modified Timer

--- My design Home page/js/coundown-timer.js
+++ My design Home page/js/coundown-timer.js
@@ -1,73 +1,41 @@
-(function($) {
-	$.fn.countdown = function(options, callback) {
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
-		//custom 'this' selector
-		thisEl = $(this);
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
 
-		//array of custom settings
-		var settings = { 
-			'date': null,
-			'format': null
-		};
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
 
-		//append the settings array to options
-		if(options) {
-			$.extend(settings, options);
-		}
-		
-		//main countdown function
-		function countdown_proc() {
-			
-			eventDate = Date.parse(settings['date']) / 1000;
-			currentDate = Math.floor($.now() / 1000);
-			
-			if(eventDate <= currentDate) {
-				callback.call(this);
-				clearInterval(interval);
-			}
-			
-			seconds = eventDate - currentDate;
-			
-			days = Math.floor(seconds / (60 * 60 * 24)); //calculate the number of days
-			seconds -= days * 60 * 60 * 24; //update the seconds variable with no. of days removed
-			
-			hours = Math.floor(seconds / (60 * 60));
-			seconds -= hours * 60 * 60; //update the seconds variable with no. of hours removed
-			
-			minutes = Math.floor(seconds / 60);
-			seconds -= minutes * 60; //update the seconds variable with no. of minutes removed
-			
-			//conditional Ss
-			if (days == 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
-			if (hours == 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
-			if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
-			if (seconds == 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
-			
-			//logic for the two_digits ON setting
-			if(settings['format'] == "on") {
-				days = (String(days).length >= 2) ? days : "0" + days;
-				hours = (String(hours).length >= 2) ? hours : "0" + hours;
-				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
-				seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
-			}
-			
-			//update the countdown's html values.
-			if(!isNaN(eventDate)) {
-				thisEl.find(".days").text(days);
-				thisEl.find(".hours").text(hours);
-				thisEl.find(".minutes").text(minutes);
-				thisEl.find(".seconds").text(seconds);
-			} else { 
-				alert("Invalid date. Here's an example: 12 Tuesday 2012 17:30:00");
-				clearInterval(interval); 
-			}
-		}
-		
-		//run the function
-		countdown_proc();
-		
-		//loop the function
-		interval = setInterval(countdown_proc, 1000);
-		
-	}
-}) (jQuery);
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 29 * 24 * 60 * 60 * 1000);
+initializeClock('countdown', deadline);
