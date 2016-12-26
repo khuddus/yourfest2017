53f430a9ae8128116d6e4bbda3d20a04b0aff620
DChanakya <dandamudichanakya@gmail.com>
2016-12-26 13:35:49 +0530

    Second version of site

---
+++ My design Home page/js/main.js
@@ -0,0 +1,150 @@
+jQuery(function($) {'use strict',
+	
+	//Countdown js
+	 $("#countdown").countdown({
+			date: "10 july 2014 12:00:00",
+			format: "on"
+		},
+		
+		function() {
+			// callback function
+		});
+	
+
+	
+	//Scroll Menu
+
+	function menuToggle()
+	{
+		var windowWidth = $(window).width();
+
+		if(windowWidth > 767 ){
+			$(window).on('scroll', function(){
+				if( $(window).scrollTop()>405 ){
+					$('.main-nav').addClass('fixed-menu animated slideInDown');
+				} else {
+					$('.main-nav').removeClass('fixed-menu animated slideInDown');
+				}
+			});
+		}else{
+			
+			$('.main-nav').addClass('fixed-menu animated slideInDown');
+				
+		}
+	}
+
+	menuToggle();
+	
+	
+	// Carousel Auto Slide Off
+	$('#event-carousel, #twitter-feed, #sponsor-carousel ').carousel({
+		interval: false
+	});
+
+
+	// Contact form validation
+	var form = $('.contact-form');
+	form.submit(function () {'use strict',
+		$this = $(this);
+		$.post($(this).attr('action'), function(data) {
+			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+		},'json');
+		return false;
+	});
+
+	$( window ).resize(function() {
+		menuToggle();
+	});
+
+	$('.main-nav ul').onePageNav({
+		currentClass: 'active',
+	    changeHash: false,
+	    scrollSpeed: 900,
+	    scrollOffset: 0,
+	    scrollThreshold: 0.3,
+	    filter: ':not(.no-scroll)'
+	});
+
+});
+
+
+// Google Map Customization
+(function(){
+
+	var map;
+
+	map = new GMaps({
+		el: '#gmap',
+		lat: 16.577756,
+		lng: 80.874477,
+		scrollwheel:false,
+		zoom: 16,
+		zoomControl : false,
+		panControl : false,
+		streetViewControl : false,
+		mapTypeControl: false,
+		overviewMapControl: false,
+		clickable: false
+	});
+
+	var image = 'images/map-icon.png';
+	map.addMarker({
+		lat: 16.577756,
+		lng: 80.874477,
+		icon: image,
+		animation: google.maps.Animation.DROP,
+		verticalAlign: 'bottom',
+		horizontalAlign: 'center',
+		backgroundColor: '#3e8bff',
+	});
+
+
+	var styles = [ 
+
+	{
+		"featureType": "road",
+		"stylers": [
+		{ "color": "#b4b4b4" }
+		]
+	},{
+		"featureType": "water",
+		"stylers": [
+		{ "color": "#d8d8d8" }
+		]
+	},{
+		"featureType": "landscape",
+		"stylers": [
+		{ "color": "#f1f1f1" }
+		]
+	},{
+		"elementType": "labels.text.fill",
+		"stylers": [
+		{ "color": "#000000" }
+		]
+	},{
+		"featureType": "poi",
+		"stylers": [
+		{ "color": "#d9d9d9" }
+		]
+	},{
+		"elementType": "labels.text",
+		"stylers": [
+		{ "saturation": 1 },
+		{ "weight": 0.1 },
+		{ "color": "#000000" }
+		]
+	}
+
+	];
+
+	map.addStyle({
+		styledMapName:"Styled Map",
+		styles: styles,
+		mapTypeId: "map_style"  
+	});
+
+	map.setStyle("map_style");
+}());
+
+
+
--- README.md
+++
@@ -1,9 +0,0 @@
-# yourfest2017
-Usharama College of Engineering Fest Website
-The website is developed to present the details of yoUR Fest 2017 and make the registrations through online conviniently.
-We took the help of Google forms to make the process of online registrations a cakewalk.
-You can visit our site at https://usharama.edu.in/yourfest/.
-This is developed by using template provided by Codrops which is available in URL "https://tympanus.net/Freebies/Boxify/"
-
-Android app available at https://play.google.com/store/apps/details?id=com.usharama.yourfest
-
