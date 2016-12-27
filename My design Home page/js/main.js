e28a929c49a5279d1a156471d614391a4b4796bc
DChanakya <dandamudichanakya@gmail.com>
2016-12-27 12:47:21 +0530

    Created technical page

--- My design Home page/js/main.js
+++ My design Home page/js/main.js
@@ -146,5 +146,11 @@ jQuery(function($) {'use strict',
 	map.setStyle("map_style");
 }());
 
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById('myPopup');
+    popup.classList.toggle('show');
+}
+
 
 
